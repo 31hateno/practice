7fc8c20ea065692f6df91d809707238723949795
31hateno <123566183+31hateno@users.noreply.github.com>
2023-07-09 13:57:19 +0900

    Renewal

--- 2023-07-09.js
+++ 2023-07-09.js
@@ -112,14 +112,27 @@ assessmentButton.onclick = () => {
   if (!userName) return;
 
   resultDiv.innerText = '';
-  const header = document.createElement('h3');
-  header.innerText = '診断結果';
-  resultDiv.appendChild(header);
+  // headerDivision の作成
+  const headerDivision = document.createElement('div');
+  headerDivision.setAttribute('class', 'card-header text-bg-primary');
+  headerDivision.innerText = 'Bad Boy...';
+
+  // bodyDivision の作成
+  const bodyDivision = document.createElement('div');
+  bodyDivision.setAttribute('class', 'card-body');
 
   const paragraph = document.createElement('p');
+  paragraph.setAttribute('class', 'card-text');
   const result = assessment(userName);
   paragraph.innerText = result;
-  resultDiv.appendChild(paragraph);
+  bodyDivision.appendChild(paragraph);
+
+  // resultDivision に Bootstrap のスタイルを適用する
+  resultDiv.setAttribute('class', 'card');
+
+  // headerDivision と bodyDivision を resultDivision に差し込む
+  resultDiv.appendChild(headerDivision);
+  resultDiv.appendChild(bodyDivision);
 
   tweetDiv.innerText = '';
   const anchor = document.createElement('a');
@@ -143,22 +156,22 @@ userNameInput.onkeydown = event => {
 }
 
 const answers = [
-  '###userName###のいいところは声です。###userName###の特徴的な声は皆を惹きつけ、心に残ります。',
-  '###userName###のいいところはまなざしです。###userName###に見つめられた人は、気になって仕方がないでしょう。',
-  '###userName###のいいところは情熱です。###userName###の情熱に周りの人は感化されます。',
-  '###userName###のいいところは厳しさです。###userName###の厳しさがものごとをいつも成功に導きます。',
-  '###userName###のいいところは知識です。博識な###userName###を多くの人が頼りにしています。',
-  '###userName###のいいところはユニークさです。###userName###だけのその特徴が皆を楽しくさせます。',
-  '###userName###のいいところは用心深さです。###userName###の洞察に、多くの人が助けられます。',
-  '###userName###のいいところは見た目です。内側から溢れ出る###userName###の良さに皆が気を惹かれます。',
-  '###userName###のいいところは決断力です。###userName###がする決断にいつも助けられる人がいます。',
-  '###userName###のいいところは思いやりです。###userName###に気をかけてもらった多くの人が感謝しています。',
-  '###userName###のいいところは感受性です。###userName###が感じたことに皆が共感し、わかりあうことができます。',
-  '###userName###のいいところは節度です。強引すぎない###userName###の考えに皆が感謝しています。',
-  '###userName###のいいところは好奇心です。新しいことに向かっていく###userName###の心構えが多くの人に魅力的に映ります。',
-  '###userName###のいいところは気配りです。###userName###の配慮が多くの人を救っています。',
-  '###userName###のいいところはその全てです。ありのままの###userName###自身がいいところなのです。',
-  '###userName###のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる###userName###が皆から評価されています。'
+  '###userName###のいいところは声ではありません。###userName###の特徴的な声は皆を惹きつけず、心に残りません。',
+  '###userName###のいいところはまなざしではありません。###userName###に見つめられた人は、気にならず仕方がないでしょう。',
+  '###userName###のいいところは情熱ではありません。###userName###の情熱に周りの人は感化されません。',
+  '###userName###のいいところは厳しさではありません。###userName###の厳しさがものごとをいつも成功に導きません。',
+  '###userName###のいいところは知識ではありません。博識な###userName###を多くの人が頼りにしていません。',
+  '###userName###のいいところはユニークさではありません。###userName###だけのその特徴が皆を楽しくさせません。',
+  '###userName###のいいところは用心深さではありません。###userName###の洞察に、多くの人が助けられません。',
+  '###userName###のいいところは見た目ではありません。内側から溢れ出る###userName###の良さに皆が気を惹かれません。',
+  '###userName###のいいところは決断力ではありません。###userName###がする決断にいつも助けられる人がいません。',
+  '###userName###のいいところは思いやりではありません。###userName###に気をかけてもらった多くの人が感謝していません。',
+  '###userName###のいいところは感受性ではありません。###userName###が感じたことに皆が共感し、わかりあうことができません。',
+  '###userName###のいいところは節度ではありません。強引すぎない###userName###の考えに皆が感謝していません。',
+  '###userName###のいいところは好奇心ではありません。新しいことに向かっていく###userName###の心構えが多くの人に魅力的に映りません。',
+  '###userName###のいいところは気配りではありません。###userName###の配慮が多くの人を救っていません。',
+  '###userName###のいいところはその全てではありません。ありのままの###userName###自身がいいところなのではありません。',
+  '###userName###のいいところは自制心ではありません。やばいと思ったときにしっかりと衝動を抑えられる###userName###が皆から評価されていません。'
 ];
 
 /**
